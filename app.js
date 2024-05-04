7e081e06b9530635a0e7c1b920a73300d8f66492
leeseul0919 <108906872+leeseul0919@users.noreply.github.com>
2024-05-05 05:23:12 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const express = require('express')
+const http = require('http');
+const WebSocket = require('ws');
+const { MongoClient } = require('mongodb');
+
 const app = express()
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 const port = 3000
+
+const MONGODB_URI = "mongodb://capstone:20211275@atlas-sql-662e8edb701bc6704f5f31ae-oj1se.a.query.mongodb.net/test?ssl=true&authSource=admin";
+const DATABASE_NAME = "test";
+const COLLECTION_NAME = "obstacles";
+
+const client = new MongoClient(MONGODB_URI, { useUnifiedTopology: true });
+wss.on('connection', function connection(ws) {
+  console.log('Client connected');
+
+  ws.on('close', function() {
+    console.log('Client disconnected');
+  });
+});
 
 app.get('/', (req, res) => {
   res.send('Hello, Cloudtype!')
