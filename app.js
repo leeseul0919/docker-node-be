26005eec10bc60f5b6a2d1502798caede9c57485
leeseul0919 <108906872+leeseul0919@users.noreply.github.com>
2024-05-06 07:25:59 +0900

    Update app.js

--- app.js
+++ app.js
@@ -8,56 +8,50 @@ const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 const port = 3000
 
-const MONGODB_URI = "mongodb://capstone:20211275@atlas-sql-662e8edb701bc6704f5f31ae-oj1se.a.query.mongodb.net/test?ssl=true&authSource=admin";
-const DATABASE_NAME = "test";
-const COLLECTION_NAME = "obstacles";
+const MongoClient = require('mongodb').MongoClient;
+var db;
+const DB_URL = "mongodb://capstone:20211275@atlas-sql-662e8edb701bc6704f5f31ae-oj1se.a.query.mongodb.net/test?ssl=true&authSource=admin"
 
-const client = new MongoClient(MONGODB_URI, { useUnifiedTopology: true });
-wss.on('connection', function connection(ws) {
-  console.log('Client connected');
-
-  ws.on('close', function() {
-    console.log('Client disconnected');
-  });
-});
+MongoClient.connect(DB_URL, function(err,client){
+    if(err) return console.log(err);
+    db = client.db('test');
+    console.log('mongodb connect')
+    watchCollectionChanges();
+})
 
 async function watchCollectionChanges() {
-  await client.connect();
-  const db = client.db(DATABASE_NAME);
   const collection = db.collection(COLLECTION_NAME);
+    const changeStream = collection.watch();
 
-  const changeStream = collection.watch();
+    changeStream.on('change', (changeEvent) => {
+        console.log("Change detected in collection:", changeEvent);
 
-  changeStream.on('change', (changeEvent) => {
-    res.send('change')
-    console.log("Change detected in collection:", changeEvent);
+        let dataToSend;
 
-    let dataToSend;
+        if (changeEvent.operationType === "insert") {
+            dataToSend = {
+                st: 1,
+                obs_id: changeEvent.fullDocument.obs_id,
+                start_x: changeEvent.fullDocument.start_x,
+                start_z: changeEvent.fullDocument.start_z,
+                end_x: changeEvent.fullDocument.end_x,
+                end_z: changeEvent.fullDocument.end_z
+            };
+        } else if (changeEvent.operationType === "delete") {
+            dataToSend = {
+                st: 2,
+                obs_id: changeEvent.documentKey.obs_id
+            };
+        }
 
-    if (changeEvent.operationType === "insert") {
-      dataToSend = {
-        st: 1,
-        obs_id: changeEvent.fullDocument.obs_id,
-        start_x: changeEvent.fullDocument.start_x,
-        start_z: changeEvent.fullDocument.start_z,
-        end_x: changeEvent.fullDocument.end_x,
-        end_z: changeEvent.fullDocument.end_z
-      };
-    } else if (changeEvent.operationType === "delete") {
-      dataToSend = {
-        st: 2,
-        obs_id: changeEvent.documentKey.obs_id
-      };
-    }
-
-    // 클라이언트에게 메시지 전송
-    const message = JSON.stringify(dataToSend);
-    wss.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(message);
-      }
+        // 클라이언트에게 메시지 전송
+        const message = JSON.stringify(dataToSend);
+        wss.clients.forEach(function each(client) {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
     });
-  });
 }
 
 app.get('/', (req, res) => {
@@ -67,5 +61,4 @@ app.get('/', (req, res) => {
 app.listen(port, async () => {
   console.log(`Example app listening on port ${port}`)
   console.log('start')
-  await watchCollectionChanges();
 })
