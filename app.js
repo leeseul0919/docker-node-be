2923171be0218bc7dd11a33389a8517787cfbfe7
leeseul0919 <108906872+leeseul0919@users.noreply.github.com>
2024-05-06 20:16:44 +0900

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const wss = new WebSocket.Server({ server });
 const port = 3000;
 
 // MongoDB 연결 설정
-const DB_URL = "mongodb+srv://capstone:20211275@cluster0.ynjxsbf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const DB_URL = "mongodb+srv://OS:MZWl4yS6ylx53ouQ@os.xcm3kqz.mongodb.net/?retryWrites=true&w=majority&appName=OS";
 mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('MongoDB connected');
